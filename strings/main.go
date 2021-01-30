62bdf5f2f85033b9f6a006a9357027e1e5843e55
Gagandeep Singh <gagan.ddun@gmail.com>
2021-01-30 11:57:54 +1100

    String to byte

--- strings/main.go
+++ strings/main.go
@@ -6,4 +6,6 @@ func main() {
 	b := []byte{104, 101, 121}
 	fmt.Println(b)         // print bytes
 	fmt.Println(string(b)) // prints string equivalent of bytes array
+
+	fmt.Println([]byte("hey")) // string to byte
 }
