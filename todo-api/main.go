7ca10fe1ab891dbd9b4c2eabff245b0a20cefe7d
Gagandeep <gagan.ddun@gmail.com>
2022-02-26 22:14:48 +1100

    get todo by id

--- todo-api/main.go
+++ todo-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -22,6 +23,28 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func getTodoById(id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
 func addTodo(context *gin.Context) {
 	var newTodo todo
 
@@ -37,6 +60,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo)
 	router.POST("/todos", addTodo)
 
 	router.Run("localhost:9090")
