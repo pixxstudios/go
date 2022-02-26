208a3d7e9f3a17a5e7ae8cb16579670f831029e4
Gagandeep <gagan.ddun@gmail.com>
2022-02-26 21:38:31 +1100

    post - todos

--- todo-api/main.go
+++ todo-api/main.go
@@ -22,10 +22,22 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 
 	router.Run("localhost:9090")
 }
