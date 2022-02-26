70470ab92749e68a41b90c5f0476b8a5c0038411
Gagandeep <gagan.ddun@gmail.com>
2022-02-26 20:33:12 +1100

    get todos

--- todo-api/main.go
+++ todo-api/main.go
@@ -1,15 +1,31 @@
 package main
 
-func main() {
-	type todo struct {
-		ID        string `json:"id"`
-		Item      string `json:"item"`
-		Completed bool   `json:"completed"`
-	}
+import (
+	"net/http"
 
-	todos := []todo{
-		{ID: "1", Item: "Clean Room", Completed: false},
-		{ID: "2", Item: "Read Book", Completed: false},
-		{ID: "3", Item: "Record Video", Completed: false},
-	}
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/todos", getTodos)
+
+	router.Run("localhost:9090")
 }
